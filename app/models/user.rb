7057f8703c445b59a74ea7d88fd49bf7545d513d
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-14 12:57:12 -0300

    arrumando formulario com pleto

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
 
   def formulario_completo?
-    unless self.name.blank? or self.telephone_home.blank? or self.telephone_cell.blank? or self.district.blank? or self.address.blank? or self.city.blank? or self.state.blank? or self.postal_code.blank? or self.email.blank? or self.waste_generated.blank? or self.type_of_company.blank?
+    unless self.name.blank? or self.telephone_home.blank? or self.telephone_cell.blank? or self.district.blank? or self.address.blank? or self.city.blank? or self.state.blank? or self.postal_code.blank? or self.email.blank? or self.waste_generated.blank? or self.type_of_user.blank?
       return true
     end
       return false

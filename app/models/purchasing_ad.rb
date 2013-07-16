cbf8b768c2a82269db763e78d97827439dad61bc
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-16 13:36:22 -0300

    steps for puc

--- app/models/purchasing_ad.rb
+++ app/models/purchasing_ad.rb
@@ -1,3 +1,4 @@
 class PurchasingAd < ActiveRecord::Base
+  has_steps :steps => %w{ waste_description title_details price_quantity_frequency sample_transport_address }
   attr_accessible :category_id, :title, :user_type
 end

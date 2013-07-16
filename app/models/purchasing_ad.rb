02a57cc62db794a7f26d5e035ea330a06147dbea
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-16 13:42:48 -0300

    valides

--- app/models/purchasing_ad.rb
+++ app/models/purchasing_ad.rb
@@ -12,4 +12,25 @@ class PurchasingAd < ActiveRecord::Base
     self.validates_presence_of :waste_origin
     self.validates_presence_of :details
   end
+
+  def validate_price_quantity_frequency
+    self.validates_presence_of :quantity
+    self.validates_presence_of :unit
+    self.validates_presence_of :unit_price
+    self.validates_presence_of :total_price
+    self.validates_presence_of :show_price
+    self.validates_presence_of :frequency_removal_waste
+    self.validates_presence_of :contract_time
+  end
+
+  def validate_sample_transport_address
+    self.validates_presence_of :today_waste
+    self.validates_presence_of :transportation_sample
+    self.validates_presence_of :waste_address
+    self.validates_presence_of :postal_code
+    self.validates_presence_of :address_complete
+    self.validates_presence_of :district
+    self.validates_presence_of :state
+    self.validates_presence_of :city
+  end
 end

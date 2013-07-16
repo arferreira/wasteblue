63c1e72a3d13d823e408307364445e5c79f5c821
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-16 13:48:03 -0300

    comment purch ads actions

--- app/controllers/purchasing_ads_controller.rb
+++ app/controllers/purchasing_ads_controller.rb
@@ -39,35 +39,35 @@ class PurchasingAdsController < ApplicationController
 
   # POST /purchasing_ads
   # POST /purchasing_ads.json
-  def create
-    @purchasing_ad = PurchasingAd.new(params[:purchasing_ad])
+  #def create
+  #  @purchasing_ad = PurchasingAd.new(params[:purchasing_ad])
 
-    respond_to do |format|
-      if @purchasing_ad.save
-        format.html { redirect_to @purchasing_ad, notice: 'Purchasing ad was successfully created.' }
-        format.json { render json: @purchasing_ad, status: :created, location: @purchasing_ad }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @purchasing_ad.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #  respond_to do |format|
+  #    if @purchasing_ad.save
+  #      format.html { redirect_to @purchasing_ad, notice: 'Purchasing ad was successfully created.' }
+  #      format.json { render json: @purchasing_ad, status: :created, location: @purchasing_ad }
+  #    else
+  #      format.html { render action: "new" }
+  #      format.json { render json: @purchasing_ad.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   # PUT /purchasing_ads/1
   # PUT /purchasing_ads/1.json
-  def update
-    @purchasing_ad = PurchasingAd.find(params[:id])
+  #def update
+  #  @purchasing_ad = PurchasingAd.find(params[:id])
 
-    respond_to do |format|
-      if @purchasing_ad.update_attributes(params[:purchasing_ad])
-        format.html { redirect_to @purchasing_ad, notice: 'Purchasing ad was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @purchasing_ad.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #  respond_to do |format|
+  #    if @purchasing_ad.update_attributes(params[:purchasing_ad])
+  #      format.html { redirect_to @purchasing_ad, notice: 'Purchasing ad was successfully updated.' }
+  #      format.json { head :no_content }
+  #    else
+  #      format.html { render action: "edit" }
+  #      format.json { render json: @purchasing_ad.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   # DELETE /purchasing_ads/1
   # DELETE /purchasing_ads/1.json

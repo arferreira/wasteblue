968bb5bd7910bd5fa5643058df747fecfc92fd65
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-18 18:38:45 -0300

    desisto

--- app/controllers/purchasing_ad_steps_controller.rb
+++ app/controllers/purchasing_ad_steps_controller.rb
@@ -2,6 +2,12 @@ class PurchasingAdStepsController < ApplicationController
   include Wicked::Wizard
   steps :waste_description, :title_details, :price_quantity_frequency, :sample_transport_address
   
+  def waste_description
+    @purchasing_ad = PurchasingAd.new
+    @categories = Category.all
+    render_wizard
+  end
+
   def show
     @purchasing_ad = PurchasingAd.new
     @categories = Category.all

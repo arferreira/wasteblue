0f50870ab904cc120dec14ebc020c5e9079497e0
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-18 15:10:18 -0300

    arrumando a casa com wicjked

--- app/controllers/purchasing_ad_steps_controller.rb
+++ app/controllers/purchasing_ad_steps_controller.rb
@@ -3,6 +3,8 @@ class PurchasingAdStepsController < ApplicationController
   steps :waste_description, :title_details, :price_quantity_frequency, :sample_transport_address
   
   def show
+    @purchasing_ad = PurchasingAd.new
+    @categories = Category.all
     render_wizard
   end
 end

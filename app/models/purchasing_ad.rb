9c948755dce89ebafb3682cd65aaed7c63ce0dbb
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-30 12:40:39 -0300

    mechida no dorm

--- app/models/purchasing_ad.rb
+++ app/models/purchasing_ad.rb
@@ -1,4 +1,4 @@
 class PurchasingAd < ActiveRecord::Base
   belongs_to :category  
-  attr_accessible :category_id, :title, :user_type, :waste_origin, :details, :quantity,:unit,:unit_price,:total_price,:show_price,:frequency_removal_waste,:contract_time,:today_waste,:transportation_sample,:waste_address,:postal_code,:address_complete,:district,:state,:city,:current_step,:waste_name
+  attr_accessible :same_address, :category_id, :title, :user_type, :waste_origin, :details, :quantity,:unit,:unit_price,:total_price,:show_price,:frequency_removal_waste,:contract_time,:today_waste,:transportation_sample,:waste_address,:postal_code,:address_complete,:district,:state,:city,:current_step,:waste_name
 end

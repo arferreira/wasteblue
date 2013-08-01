2d7f6eab5c0689b06afe2cba545e833afd12919c
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-31 23:59:58 -0300

    subimage and new thumbnail

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -19,6 +19,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   version :thumb do
     process :resize_to_fill => [140, 140]
   end
+  version :throbber do
+    process :resize_to_fill => [220, 220]
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
